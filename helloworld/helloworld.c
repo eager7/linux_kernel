037749aa4cd097c050baf6140f6f8ef2e09832b4
eager7 <panchagntao@gmail.com>
2017-01-02 21:23:52 -0800

    1. 为hello world工程增加传入参数的功能，使用方法如下：      sudo insmod how_many=10 whom="test"

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -2,10 +2,22 @@
 #include <linux/module.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Changtao.Pan");
+MODULE_VERSION("1.0");
+MODULE_DESCRIPTION("hello world");
+
+static int how_many = 0;
+static char *whom = "pct";
+module_param(how_many, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
 
 static int hello_init(void)
 {
-    printk(KERN_ALERT "hello init\n");
+ 	int i = 0;
+	printk(KERN_ALERT "hello init\n");
+	for(i = 0; i < how_many; i++){
+		printk(KERN_DEBUG "hello %s\n", whom);
+	}
     return 0;
 }
 
